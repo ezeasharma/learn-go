c90c42a86f59854b6c479eb9ea9fe996bcc52e4b
Ashish Sharma <asharma@ezesoft.com>
2016-05-25 17:25:23 +0530

    using inspire me as source

--- src/request-handler.go
+++ src/request-handler.go
@@ -5,24 +5,18 @@ import (
     "fmt"
     "io/ioutil"
      "log"
-     //"encoding/json"
+
 )
 
 //HandleQuoteRequest will be responsible for handling all the web requests.
 func HandleQuoteRequest(responseWrite http.ResponseWriter, request *http.Request)  {
     log.Println("Got request!")
-    client := &http.Client{}
-    req, _ := http.NewRequest("POST", "https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies", nil)
-    req.Header.Set("X-Mashape-Key", "e3PM6jUqiYmsh3RbYScbSI8NgkpUp1hLBBXjsnJ6kJq7LUOnU3")
-    resp, err := client.Do(req);
+    resp, err := http.Get("http://inspireme.ezecloud.net")
     //resp, err := http.Get("https://google.com")
     if err != nil {
         fmt.Fprintf(responseWrite, "Error connecting to InspireMe server %s", err)
     }  else {
-        
-        
         htmlData, _ := ioutil.ReadAll(resp.Body)
-        
         fmt.Fprintf(responseWrite, string(htmlData))   
     }
 }
